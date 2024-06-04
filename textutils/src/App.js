742df6b165c0515a97b1d7c4371457013c0105f1
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-06-04 09:00:42 +0545

    updte on darkMode

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,18 +1,27 @@
 import './App.css';
+import Alert from './components/Alert';
 import Navbar from './components/Navbar';
 import TextBox from './components/TextBox';
-// import About from './components/About';
+//import About from './components/About';
 import React, {useState} from 'react';
 
 function App() {
   const [mode, setMode] = useState('light');
   const [btnTxt, setBtnTxt] = useState('Enable Dark Mode');
+  const [alert, setAlert] = useState(null)
+
+  const showAlert = (message, type) =>{
+           setAlert({
+            msg:message,
+            type:type
+           })
+  }
 
   const toggleMode = ()=> {
     if(mode === 'light'){
       setMode("dark");
       setBtnTxt("Disable Dark Mode");
-      document.body.style.backgroundColor = 'grey';
+      document.body.style.backgroundColor = '#042743';
     }
     else{
       setMode("light")
@@ -24,8 +33,9 @@ function App() {
   return (
     <>
     <Navbar  forLogo = "LOGO" mode = {mode} toggleMode={toggleMode} btnTxt={btnTxt}/>
+    <Alert alert="This is Alert" />
       <div className='container my-3'>
-      <TextBox  heading="Heading goes here"/>
+      <TextBox  heading="Heading goes here" mode={mode}/>
       {/* <About/> */}
       </div>
     </>
--- textutils/src/components/TextBox.js
+++ textutils/src/components/TextBox.js
@@ -1,41 +1,64 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 export default function TextBox(props) {
-let handleUpClick = () =>{
-    console.log("Button was clicked")
+  let handleUpClick = () => {
+    console.log("Button was clicked");
     let newText = text.toUpperCase();
     setText(newText);
-}
+  };
 
-let handleLoClick = () =>{
-    console.log("Button was clicked")
+  let handleLoClick = () => {
+    console.log("Button was clicked");
     let newText = text.toLowerCase();
     setText(newText);
-}
+  };
 
-let handleOnChange = (event) =>{
-    setText(event.target.value)
-}
+  let handleOnChange = (event) => {
+    setText(event.target.value);
+  };
 
-    const [text, setText] = useState('Enter text here...');
+  const [text, setText] = useState("Enter text here...");
   return (
     <>
-    <div className='container' style={{color : props.mode==='dark'?'white':'black'}}>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
         <h1>{props.heading}</h1>
-<div className="mb-3">
-  <textarea className="form-control" style={{backgroundColor : props.mode==='dark'?'grey':'white',color : props.mode==='dark'?'white':'black'}} id="myBox" rows="8" value={text} onChange={handleOnChange}></textarea>
-</div>
-<div className="btn btn-primary mx-1" onClick={handleUpClick}>Convert To UpperCase</div>
-<div className="btn btn-primary mx-1" onClick={handleLoClick}>Convert To LowerCase</div>
-    </div>
+        <div className="mb-3">
+          {console.log(props.mode)}
+          <textarea
+            className="form-control"
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+              color: props.mode === "dark" ? "white" : "black",
+            }}
+            id="myBox"
+            rows="8"
+            value={text}
+            onChange={handleOnChange}
+          ></textarea>
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert To UpperCase
+        </div>
+        <div className="btn btn-primary mx-1" onClick={handleLoClick}>
+          Convert To LowerCase
+        </div>
+      </div>
 
-    <div className="container">
-      <h1>Your text summary</h1>
-      <p>{text.split(" ").length} words and {text.length} characters</p>
-      <p>{0.008 * text.split(" ").length } Minutes Read</p>
-      <h2>Preview</h2>
-      <p>{text}</p>
-    </div>
+      <div
+        className="container"
+        style={{ color: props.mode === "dark" ? "white" : "black" }}
+      >
+        <h1>Your text summary</h1>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h2>Preview</h2>
+        <p>{text.length > 0 ? text : "Enter some text to preview it here"}</p>
+      </div>
     </>
-  )
+  );
 }
