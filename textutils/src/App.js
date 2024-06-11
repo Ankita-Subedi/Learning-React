bb41a6f7008ead3082e0c9c93e3d69a49f0891c6
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-06-11 08:32:45 +0545

    remove route and deploy

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,88 +1,74 @@
-import './App.css';
-import Alert from './components/Alert';
-import Navbar from './components/Navbar';
-import TextBox from './components/TextBox';
-import About from './components/About';
-import React, {useState} from 'react';
-// import { Router } from 'react-router-dom';
-// import React from "react";
-import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-} from "react-router-dom";
-
+import "./App.css";
+import Alert from "./components/Alert";
+import Navbar from "./components/Navbar";
+import TextBox from "./components/TextBox";
+//import About from "./components/About";
+import React, { useState } from "react";
+//import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 
 function App() {
-  const [mode, setMode] = useState('light');
-  const [btnTxt, setBtnTxt] = useState('Enable Dark Mode');
-  const [alert, setAlert] = useState(null)
+  const [mode, setMode] = useState("light");
+  const [btnTxt, setBtnTxt] = useState("Enable Dark Mode");
+  const [alert, setAlert] = useState(null);
 
-  const showAlert = (message, type) =>{
-           setAlert({
-            msg:message,
-            type:type
+  const showAlert = (message, type) => {
+    setAlert({
+      msg: message,
+      type: type,
+    });
+    setTimeout(() => {
+      setAlert(null);
+    }, 1500);
+  };
 
-          })
-          setTimeout(() => {
-            setAlert(null)
-          }, 1500);
-  }
-
-  const toggleMode = ()=> {
-    if(mode === 'light'){
+  const toggleMode = () => {
+    if (mode === "light") {
       setMode("dark");
       setBtnTxt("Disable Dark Mode");
-      document.body.style.backgroundColor = '#042743';
+      document.body.style.backgroundColor = "#042743";
       showAlert("Dark mode enabled", "success");
-      document.title = 'TextUtils-Dark Mode';
-    }
-    else{
-      setMode("light")
-      setBtnTxt("Enable Dark Mode")
-      document.body.style.backgroundColor = 'white';
+      document.title = "TextUtils-Dark Mode";
+    } else {
+      setMode("light");
+      setBtnTxt("Enable Dark Mode");
+      document.body.style.backgroundColor = "white";
       showAlert("Light mode enabled", "success");
-      document.title = 'TextUtils-Dark Mode';
+      document.title = "TextUtils-Dark Mode";
     }
   };
 
   return (
     <>
-      {/* <BrowserRouter>
-        <Navbar
-          title="TextUtils2"
-          aboutText="TextAbouts"
-          mode={mode}
-          toggleMode={toggleMode}
-        />
+      {/* <Router>
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
-        <div className="container my-4" mode={mode}>
+        <div className="container my-3">
           <Routes>
-            <Route exact path="/about" element={<About />}></Route>
+            <Route exact path="/about" element={<About />} />
             <Route
               exact path="/"
               element={
-                <Textbox
+                <TextBox
                   showAlert={showAlert}
-                  heading="Enter Text to analyze "
+                  heading="Enter text to analyze below"
                   mode={mode}
                 />
               }
-            ></Route>
+            />
           </Routes>
         </div>
-      </BrowserRouter> */}
+      </Router> */}
 
-<Router>
-      <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
-      <Alert alert={alert} />
-      <div className="container my-3">
-        <Routes>
-          <Route path="/about" element={<About />} />
-          <Route path="/" element={<TextBox showAlert={showAlert} heading="Enter text to analyze below" mode={mode} />} />
-        </Routes>
-      </div>
-    </Router>
+
+        <Navbar title="TextUtils" mode={mode} toggleMode={toggleMode} />
+        <Alert alert={alert} />
+        <div className="container my-3">
+                <TextBox
+                  showAlert={showAlert}
+                  heading="Enter text to analyze below"
+                  mode={mode}
+                />
+        </div>
     </>
   );
 }
