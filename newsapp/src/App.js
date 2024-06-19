fc3cfa045e25c6b7cb8af14707c44abe881eba98
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-06-19 22:03:54 +0545

    updated

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 
 import React, { Component } from 'react'
+import NewsItem from './components/NewsItem';
+import NavBar from './components/NavBar';
 
 export default class App extends Component {
   render() {
     return (
       <div>
-        My first class based component
+        <NavBar/>
       </div>
     )
   }
