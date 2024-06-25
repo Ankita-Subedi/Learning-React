5c12653f8bfb1055ed54f4edbab820a2391fd22b
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-06-25 09:58:12 +0545

    add News and NewsItem component

--- newsapp/src/App.js
+++ newsapp/src/App.js
@@ -3,12 +3,14 @@ import './App.css';
 import React, { Component } from 'react'
 import NewsItem from './components/NewsItem';
 import NavBar from './components/NavBar';
+import News from './components/News';
 
 export default class App extends Component {
   render() {
     return (
       <div>
         <NavBar/>
+        <News/>
       </div>
     )
   }
