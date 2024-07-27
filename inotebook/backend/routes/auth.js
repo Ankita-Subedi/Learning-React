d168cd16c93109c5bc87980a243a39b36d23e51a
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-07-27 15:57:25 +0545

    hash psw using bycryptjs and use JWT auth

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const router = express.Router();
 const User = require("../models/User");
+const bcrypt = require('bcryptjs')
+var jwt = require('jsonwebtoken');
 const { body, validationResult } = require("express-validator");
+
+const JWT_SECRET = "Ankitaisagooddev";
 
 //create user using: POST "/api/auth/createuser". No login required
 router.post(
@@ -24,13 +28,23 @@ router.post(
         if(user){
             return res.status(400).json({error: "Sorry user with this email aready exists."})
         }
+        
+        const salt = await bcrypt.genSaltSync(10);
+        const secPass = await bcrypt.hash(req.body.password, salt);
+        //Creating new User
         user = await User.create({
             name: req.body.name,
-            password: req.body.password,
+            password: secPass,
             email: req.body.email,
-        })
-        
-        res.json(user)
+        });
+        const data = {
+            user:{
+                id: user.id
+            }
+        }
+        const authtoken = jwt.sign(data, JWT_SECRET)
+
+        res.json({authtoken})
     }
     catch(error){
         console.error(error.message);
