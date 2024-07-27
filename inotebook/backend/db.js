9c8d81cf5d4003ea4a98dbfdaadac7f9df4cb0bc
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-07-27 14:29:52 +0545

    create new user in thunderclient

--- inotebook/backend/db.js
+++ inotebook/backend/db.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const mongoURI = 'mongodb://127.0.0.1:27017';
+const mongoURI = 'mongodb://127.0.0.1:27017/iNotebook';
 
 async function connectToMongo() {
     await mongoose.connect(mongoURI).then(()=> console.log("Connected to Mongo Successfully")).catch(err => console.log(err));
--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -1,22 +1,42 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const User = require('../models/User');
+const User = require("../models/User");
+const { body, validationResult } = require("express-validator");
 
-router.post('/', async(req, res)=> {
-    try{
-        console.log(req.body)
-    res.send(req.body);
-    const user = User(req.body)
-    user.save()
-    }catch (error) {
-        if (error.code === 11000) {
-            // Duplicate key error
-            res.status(400).send({ error: 'Email already exists' });
-        } else {
-            console.error(error);
-            res.status(500).send({ error: 'Internal Server Error' });
-        }
+//create user using: POST "/api/auth/createuser". No login required
+router.post(
+  "/createuser",
+  [
+    body("name","Enter valid name").isLength({ min: 3 }),
+    body("password").isLength({ min: 5 }),
+    body("email").isEmail(),
+  ],
+  async(req, res) => {
+    //if there are errors, return bad request and errors
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
     }
-});
 
-module.exports = router
+    //check wether user with this email exists already
+    try{
+        let user = await User.findOne({email: req.body.email});
+        if(user){
+            return res.status(400).json({error: "Sorry user with this email aready exists."})
+        }
+        user = await User.create({
+            name: req.body.name,
+            password: req.body.password,
+            email: req.body.email,
+        })
+        
+        res.json(user)
+    }
+    catch(error){
+        console.error(error.message);
+        res.status(500).send("Some error occured")
+    }
+  }
+);
+
+module.exports = router;
