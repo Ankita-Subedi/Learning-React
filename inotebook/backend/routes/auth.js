183571b431d46c230e3db5ae3c7b7816d45aecc9
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-07-27 08:36:37 +0545

    switch from get to post

--- inotebook/backend/routes/auth.js
+++ inotebook/backend/routes/auth.js
@@ -2,11 +2,21 @@ const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
 
-router.get('/', (req, res)=> {
-    console.log(req.body)
+router.post('/', async(req, res)=> {
+    try{
+        console.log(req.body)
     res.send(req.body);
     const user = User(req.body)
     user.save()
-})
+    }catch (error) {
+        if (error.code === 11000) {
+            // Duplicate key error
+            res.status(400).send({ error: 'Email already exists' });
+        } else {
+            console.error(error);
+            res.status(500).send({ error: 'Internal Server Error' });
+        }
+    }
+});
 
 module.exports = router
