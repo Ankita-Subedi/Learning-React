04d2c407e7d50fc4b8d36c8764e4f9dff19ea5d9
Ankita-Subedi <ankita7705@mbmcsit.edu.np>
2024-12-10 13:14:51 +0545

    update

--- task/src/section/Subscribe.jsx
+++ task/src/section/Subscribe.jsx
@@ -5,12 +5,12 @@ const Subscribe = () => {
   return (
     <div className="bg-customBlue px-6 py-16 sm:py-24 lg:px-36 lg:py-36 relative grid grid-cols-1 lg:grid-cols-2 gap-8">
       {/* Feedback Section */}
-      <div className="absolute left-0 bottom-0 lg:relative lg:bottom-auto lg:left-auto">
+      <div className="absolute left-0 bottom-0">
         <Feedback />
       </div>
 
       {/* Subscription Section */}
-      <div className="container flex flex-col items-start space-y-6">
+      <div className="container flex flex-col items-start space-y-6 absolute right-0">
         <h2 className="text-2xl lg:text-4xl font-bold text-white">
           Subscribe to Our Newsletter
         </h2>
